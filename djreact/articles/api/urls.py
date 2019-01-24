b0871424dbad46dcd32306fb893c6770cec72e00
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2019-01-24 17:23:15 +0700

    djreact

---
+++ djreact/articles/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ArticleListView, ArticleDetailView
+
+urlpatterns = [
+    path('<int:pk>', ArticleDetailView.as_view()),
+    path('', ArticleListView.as_view()),
+]
---
+++ djreact/gui/src/containers/ArticleDetailView.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+import { Button, Card } from 'antd';
+
+class ArticleDetail extends React.Component {
+    state = {
+        article: {}
+    };
+    
+    componentDidMount() {
+        const articleID = this.props.match.params.articleID;
+        axios.get(`http://127.0.0.1/api/${articleID}`).then(res => {
+            this.setState({
+                article: res.data
+            });
+        });
+    };
+
+    render() {
+        return (
+            <Card title={this.state.article.title}>
+                <p>{this.state.article.content}</p>
+            </Card>
+        )
+    }
+}
+
+export default ArticleDetail;
