4479aec792ebe6213b86f3c4c5a6e418ce41ffc2
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2019-05-17 00:46:41 +0700

    update dream-blog

--- dream-blog/src/settings.py
+++ dream-blog/src/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts.apps.PostsConfig',
+    'marketing.apps.MarketingConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'src/static_venv')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- dream-blog/src/urls.py
+++ dream-blog/src/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from posts.views import index, blog, post
 
 urlpatterns = [
+    path('', index),
+    path('blog/', blog),
+    path('post/', post),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
